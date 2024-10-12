c932977ac9f91567ae35013747f8053632d0e5ed
Pat <patdileti@gmail.com>
2024-10-12 00:15:42 -0300

    v

--- src/components/VoiceRecognition.js
+++ src/components/VoiceRecognition.js
@@ -11,7 +11,8 @@ const VoiceRecognition = () => {
   const recognitionRef = useRef(null);
   const chatEndRef = useRef(null);
 
-  const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+  //const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+  const API_URL = process.env.REACT_APP_API_URL;
 
 
   const SpeechRecognition =
